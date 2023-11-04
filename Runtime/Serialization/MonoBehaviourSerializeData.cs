47b5dd5e70cf9288a39dc79a646126d8d41d67ce
AkiKurisu <2683987717@qq.com>
2023-11-04 18:44:01 +0800

    Update MonoBehaviourSerializeData.cs
    Fix instanceID not work at runtime

--- Runtime/Serialization/MonoBehaviourSerializeData.cs
+++ Runtime/Serialization/MonoBehaviourSerializeData.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using Newtonsoft.Json.Linq;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -40,18 +41,52 @@ namespace Kurisu.MonoReference
         public void Deserialize(ReferencedMonoBehaviour referencedMonoBehaviour)
         {
             if (string.IsNullOrEmpty(serializeJsonData)) return;
+            JsonUtility.FromJsonOverwrite(serializeJsonData, referencedMonoBehaviour);
             JObject obj = JObject.Parse(serializeJsonData);
             var objectMap = objectSerializeMap.ToDictionary();
+            var behaviourType = referencedMonoBehaviour.GetType();
             foreach (JProperty prop in obj.Descendants().OfType<JProperty>().ToList())
             {
                 if (prop.Name == "instanceID")
                 {
                     if (!objectMap.TryGetValue((int)prop.Value, out UObject uObject)) continue;
-                    prop.Value = uObject.GetInstanceID();
+                    var field = behaviourType.FindField(prop.Path);
+                    if (field == null)
+                    {
+                        Debug.LogWarning($"Can not find field from path : {prop.Path}");
+                        continue;
+                    }
+                    field.SetValue(referencedMonoBehaviour, uObject);
                 }
             }
-            JsonUtility.FromJsonOverwrite(obj.ToString(), referencedMonoBehaviour);
         }
 
+    }
+    public static class ReflectionExtension
+    {
+        public static FieldInfo FindField(this Type sourceType, string path)
+        {
+            string[] tokens = path.Split('.');
+            FieldInfo fieldInfo = null;
+            for (int i = 0; i < tokens.Length - 1; ++i)
+            {
+                fieldInfo = sourceType.FindField(tokens[i], BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic);
+                if (fieldInfo == null)
+                {
+                    return null;
+                }
+                sourceType = fieldInfo.FieldType;
+            }
+            return fieldInfo;
+        }
+        public static FieldInfo FindField(this Type type, string fieldName, BindingFlags flags)
+        {
+            FieldInfo field = type.GetField(fieldName, flags);
+            if (field == null)
+            {
+                return type.BaseType?.FindField(fieldName, flags | BindingFlags.DeclaredOnly);
+            }
+            return field;
+        }
     }
 }
